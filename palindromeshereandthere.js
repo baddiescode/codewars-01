7d53b611f84645c993345dd2b5ca6d26cd56dbb0
baddiescode <designerjunkiegfx@gmail.com>
2023-04-27 09:02:28 -0400

    codewar 4/27

---
+++ palindromeshereandthere.js
@@ -0,0 +1,17 @@
+// An array is given with palindromic and non-palindromic numbers. 
+// A palindromic number is a number that is the same from a reversed order. 
+// For example 122 is not a palindromic number, but 202 is one.
+
+// // Your task is to write a function that returns an array with 
+// only 1s and 0s, where all palindromic numbers are replaced with a 1 and 
+// all non-palindromic numbers are replaced with a 0.
+
+function convertPalindromes(numbers) {
+    return numbers.map(arr => {
+      if(arr.toString() === arr.toString().split('').reverse().join('')){
+        return 1;
+      } else {
+        return 0;
+      }
+    })
+    };
