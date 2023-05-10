ea709e699e1320d3d58dd244cb90464a094a3209
baddiescode <designerjunkiegfx@gmail.com>
2023-05-10 11:38:02 -0400

    codewar 5/10

---
+++ reversewords.js
@@ -0,0 +1,15 @@
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+function reverseWords(str) {
+    str = str.split(' ');
+    str = str.map(x => x.split('').reverse());
+    str = str.map(x => x.join(''));
+    return str.join(' ');
+    }
+
+
+    function reverseWords(str) {
+        // Go for it
+        //split words into seperate arrays
+        return str.split("").reverse().join("").split(" ").reverse().join(" ");
+      }
